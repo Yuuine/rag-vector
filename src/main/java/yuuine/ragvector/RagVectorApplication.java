116c30de99372539458315c0e1fe661a4c2f285a
yunqing <jzhang0703@163.com>
2025-12-21 19:38:23 +0800

    initial commit

---
+++ src/main/java/yuuine/ragvector/RagVectorApplication.java
@@ -0,0 +1,13 @@
+package yuuine.ragvector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagVectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagVectorApplication.class, args);
+    }
+
+}
